1215c5f5e8b8552850eb0fc7716babd08f57e7e1
Josh Ryals <jryals@kvc.org>
2022-03-07 22:51:02 -0600

    added bootstrap/sass files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
